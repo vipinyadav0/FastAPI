a723594c4ae9aebbc7ce8d54402225039aaadc9e
Vipin Yadav <vyvipinyadav998@gmail.com>
2022-05-26 12:04:49 +0530

    Get post by ID code added

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from xmlrpc.client import Boolean
 from fastapi import FastAPI
 from fastapi.params import Body
 
+from random import randrange
+
 from pydantic import BaseModel # to create schema
 
 app = FastAPI()
@@ -27,16 +29,18 @@ def post():
     # return {"data: Here is your post"}
     return {"data": my_posts}
 
+
+
 @app.post("/createpost")  # though not the best practice to create post routing
 def userpost(payLoad: dict = Body(...)):                 # Extract Data that we send in Body section 
     print(payLoad)
     # return {"Data : Post created"}
     return { "New_Post" : f"title: {payLoad['title']}, content: {payLoad['content']}"}
 
+
+
 # Data we want for a specific post request
 # 1. Title i.e string (str) , 2. Content i.e string(str)
-
-
 # Instead of extracting data i.e Payload
 
 @app.post("/makepost")
@@ -47,14 +51,21 @@ def userpost(post_data : Post):                 # Extract Data that we send in B
     print(post_data.rating)
     
     print(post_data.dict())  #converting data into dictionary
-
-
-    
-    
     # return {"Data : Post created"}
     # return {"data" : "NewPost"}
     return {"Data" : post_data}
 
+
+
 @app.post("/posts")
 def userpost(post_data: Post):
-    return {"Data" : post_data}
+    post_dict = post_data.dict()
+    post_dict['id'] = randrange(0,1000000) 
+    my_posts.append(post_dict)
+    return {"Data" : post_dict}
+
+@app.get("/posts{id}")
+def get_post(id):
+    print(id)
+    return {"Post_datail" : f"here is the post {id}"}
+    ...
