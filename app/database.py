b6172a1b9708cf5b10f3e7acaea62e76b88c4969
Vipin Yadav <vyvipinyadav998@gmail.com>
2022-06-07 15:38:21 +0530

    database file added

---
+++ app/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# SQLALCHEMY_DATABASE_URL = "postgresql://<username>:<password>@ip_address/hostname/<db_name>"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres123@localhost/FastAPI_db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
