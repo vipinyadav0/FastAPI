5cea9fa0086c86d24d6b434392fe6ec85b0223e9
Vipin Yadav <vyvipinyadav998@gmail.com>
2022-06-07 15:38:50 +0530

    models.py added

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from email.policy import default
+from http import server
+from sqlite3 import Timestamp
+from .database import Base, engine, SessionLocal
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
+
+
+
+
+class Post(Base):
+    __tablename__ = "dbposts"
+    
+    id = Column(Integer, primary_key=True, nullable = False)
+    title = Column(String(100), nullable = False)
+    content = Column(String(1000), nullable = False)
+    published = Column(Boolean, server_default="TRUE", nullable = False) 
+    created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable = False)
