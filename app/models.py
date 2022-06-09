8db1a03eae64ec7d14b30fd601fd4b198e158041
Vipin Yadav <vyvipinyadav998@gmail.com>
2022-06-09 09:57:48 +0530

    added

--- app/models.py
+++ app/models.py
@@ -14,3 +14,5 @@ class Posts(Base):
     content = Column(String(1000), nullable = False)
     published = Column(Boolean, server_default="TRUE", nullable = False) 
     created_at = Column(TIMESTAMP(timezone=True), server_default=text('now()'), nullable = False)
+    
+#this is under db_schemas
